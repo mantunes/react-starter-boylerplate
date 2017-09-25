71db81433848f689dc27c67e0136d166e78355d0
samuthekid <samuthekid@gmail.com>
2017-09-25 16:47:43 +0100

    fix wercker css path errors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 const App = () => (
   <div className="App">
     <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
       <h2>Welcome to React</h2>
     </div>
     <p className="App-intro">
